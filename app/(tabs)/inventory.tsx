98f4295ac28dc106b9d7e1b62337495504c4a70d
Mathys Dubois <mathys.dubois@insa-rennes.fr>
2025-02-18 14:37:25 +0100

    creation de l'application, de la page d'accueil et de la page inventaire

---
+++ app/(tabs)/inventory.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Inventory() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>welcome to Inventory</Text>
+    </View>
+  );
+}
