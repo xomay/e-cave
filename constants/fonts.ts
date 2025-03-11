77d41cd61b0a5d4b7108fff1c487f2c46239b2b6
Mathys Dubois <mathys.dubois@insa-rennes.fr>
2025-03-11 22:53:45 +0100

    add only used fonts

--- constants/fonts.ts
+++ constants/fonts.ts
@@ -11,11 +11,11 @@ import * as Font from 'expo-font';
 };*/
 
 Font.loadAsync({
-    'SF-bold': require('../assets/fonts/SF_Pro/SF-Pro-Display-Bold.otf'),
-    'SF-medium': require('../assets/fonts/SF_Pro/SF-Pro-Display-Medium.otf'),
-    'SF-regular': require('../assets/fonts/SF_Pro/SF-Pro-Display-Regular.otf'),
-    'SpaceMono-regular': require('../assets/fonts/SpaceMono-Regular.ttf'),
-    'Neue-Bold': require('../assets/fonts/neue_regrade/Neue Regrade Bold.otf'),
+    'SF-bold': require('../assets/usedFonts/SF_Pro/SF-Pro-Display-Bold.otf'),
+    'SF-medium': require('../assets/usedFonts/SF_Pro/SF-Pro-Display-Medium.otf'),
+    'SF-regular': require('../assets/usedFonts/SF_Pro/SF-Pro-Display-Regular.otf'),
+    'SpaceMono-regular': require('../assets/usedFonts/SpaceMono-Regular.ttf'),
+    'Neue-Bold': require('../assets/usedFonts/neue_regrade/Neue Regrade Bold.otf'),
 })
 
 export const fonts = {
